163842b729fc5c499d4f439a2a208ad1ce5d4ebb
Justin <jbellars@gmail.com>
2023-09-05 12:52:04 -0700

    Added RoundRobin Pool example

---
+++ src/main/scala/roundRobinActor.scala
@@ -0,0 +1,22 @@
+import akka.actor.{Props, ActorSystem, Actor}
+import akka.routing.RoundRobinPool
+
+// Distributed computing example
+
+// whichever actor has the least amount of activity is used (or randomly selected if the same)
+class mailboxActor3 extends Actor {
+  override def receive = {
+    case msg: String => println(s"I am ${self.path.name}, message is: $msg")
+    case _ => println("Invalid message!")
+  }
+}
+
+object roundRobinActor extends App {
+  val actorSystem = ActorSystem("Route")
+  val router = actorSystem.actorOf(RoundRobinPool(5).props(Props[mailboxActor3]))
+
+  for (i <- 1 to 10) {
+    router!s"Hello $i"
+    // Thread.sleep(200) // <-- visibly demonstrates round robin in orderly fashion in more pronounced fashion
+  }
+}
