9ce8e3a2fc2c6825c7744792c7bc02b2f30b35f4
Justin <jbellars@gmail.com>
2023-09-05 12:48:19 -0700

    Correction to idlestActor
    change to let programs co-exist in same project

--- src/main/scala/idlestActor.scala
+++ src/main/scala/idlestActor.scala
@@ -4,7 +4,7 @@ import akka.routing.BalancingPool
 // Distributed computing example
 
 // whichever actor has the least amount of activity is used (or randomly selected if the same)
-class mailboxActor extends Actor {
+class mailboxActor2 extends Actor {
   override def receive = {
     case msg: String => println(s"I am ${self.path.name}, message is: $msg")
     case _ => println("Invalid message!")
@@ -13,7 +13,7 @@ class mailboxActor extends Actor {
 
 object idlestActor extends App {
   val actorSystem = ActorSystem("Route")
-  val router = actorSystem.actorOf(BalancingPool(5).props(Props[mailboxActor]))
+  val router = actorSystem.actorOf(BalancingPool(5).props(Props[mailboxActor2]))
 
   for (i <- 1 to 10) {
     router!s"Hello $i"
