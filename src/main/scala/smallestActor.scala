c246884fd5047dedb11577b9a50ab9430c4e8be7
Justin <jbellars@gmail.com>
2023-09-05 11:51:17 -0700

    Added smallest [mailbox] actor demo

---
+++ src/main/scala/smallestActor.scala
@@ -0,0 +1,21 @@
+import akka.actor.{Props, ActorSystem, Actor}
+import akka.routing.SmallestMailboxPool
+
+// Distributed computing example
+
+// whichever actor has the least amount of activity is used (or randomly selected if the same)
+class mailboxActor extends Actor {
+  override def receive = {
+    case msg: String => println(s"I am ${self.path.name}, message is: $msg")
+    case _ => println("Invalid message!")
+  }
+}
+
+object smallestActor extends App {
+  val actorSystem = ActorSystem("Route")
+  val router = actorSystem.actorOf(SmallestMailboxPool(5).props(Props[mailboxActor]))
+
+  for (i <- 1 to 10) {
+    router!s"Hello $i"
+  }
+}
